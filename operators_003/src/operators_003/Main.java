07d44ab50f0299cb923a2b8b6b99f84b64a58953
kadircali <a.kadircali@hotmail.com>
2024-08-06 07:33:29 +0300

    Java_book_notes

---
+++ operators_003/src/operators_003/Main.java
@@ -0,0 +1,135 @@
+package operators_003;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * assignment operators It is used to assign one variable to another variable or
+		 * to assign a value to a variable. = Assignments are right-to-left += It
+		 * assigns the value on the left by adding the value on the right to its own
+		 * value. -= += /= %=
+		 */
+
+//		int a = 5 ;
+//		int b = a ;
+//		a = a + 5 ;
+//		a +=5 ;
+//		a *=10;
+//		System.out.println(a);
+
+//		int a = 10 , b= 20, c=100,d=40;
+//		a += 5 ;
+//		b -=25 ;
+//		c *=2;
+//		d /=10 ;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
+
+		// swap
+//		int a = 5 ;
+//		int b = 10 ;
+//		int c ;
+//		c = a ; // c= 5
+//		a = b ;//a = 10
+//		b = c  ; //b = 5
+
+		/*
+		 * relational operators Relational operators are used to compare data
+		 * 
+		 * < > <= >= == != The returned data will be boolean
+		 */
+
+//		int a = 10 ;
+//		int b = 6 ;
+////		System.out.println (a > b); 
+//		boolean result = a > b ;
+//		System.out.println(result);
+//		
+//		System.out.println(5 == 6 );
+//		System.out.println(5 != 5);
+//		System.out.println(7 >= 7);
+
+		/*
+		 * 
+		 * arithmetic operator :They are used to perform mathematical operations + - * /
+		 * % ++ --
+		 */
+//		int a = 8;
+//		int b = 5;
+//
+//		int sum = a + b;
+//		int difference = a - b;
+//		int product = a * b;
+//		int quotient = a / b;
+//
+//		System.out.println("Addition: " + a + " + " + b + " = " + sum);
+//		System.out.println("Subtraction: " + a + " - " + b + " = " + difference);
+//		System.out.println("Multiplication: " + a + " * " + b + " = " + product);
+//		System.out.println("Division: " + a + " / " + b + " = " + quotient);
+
+		// %
+//		System.out.println(40 % 6); //4 
+
+		// -
+//		int x = 5 ;
+//		x = -x ;
+//		System.out.println(x);
+
+		// ++ and --
+//		int n1 = 10 ;
+//		int a,b;
+//		a = n1++;
+//		b = n1 ;
+//		System.out.println(a);
+//		System.out.println(b);
+
+//		int n1 = 20;
+//		int a, b, c, d, e, f;
+//		a = ++n1;
+//		b = n1--;
+//		c=--n1;
+//		d=++n1;
+//		e=n1++;
+//		f=n1;
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
+//		System.out.println(e);
+//		System.out.println(f);
+		
+		
+		/*
+		 * 
+		 * Logical operators
+		 * & logical and
+		 * | logical or
+		 * && conditional and
+		 * || conditional or
+		 * ! logical not
+		 * ^ xor
+		 */
+
+		
+//		int password=1234;
+//		String userName ="admin";
+//		boolean result = userName =="admin" && password == 1234;
+//		System.out.println(result);
+		
+		/*
+		 * Bitwise operators
+		 * ~ It flips all the bits
+		 * & It performs a bitwise 'AND' operation
+		 * | It performs a bitwise 'Or' operation
+		 * ^ It performs a bitwise 'xor' operation
+		 * << Bitwise left shift
+		 * >> Bitwise right shift
+		 */
+		
+//		System.out.println(3 & 14 );
+//		System.out.println(3 ^ 14);
+//		System.out.println(~3);
+	}
+}
